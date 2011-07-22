75362a51ad7f230bba56a246b3d2d23893308e6c
JR Bing <jrbing@gmail.com>
2011-07-22 11:01:50 -0400

    Cleaned up configuration prompting

--- lib/voodoo/commands/add.rb
+++ lib/voodoo/commands/add.rb
@@ -5,7 +5,7 @@ module Voodoo
 
     def self.add(args=nil)
       settings = {}
-      puts "\n### Appdesigner/Datamover/AppEngine Settings ###"
+      puts "\n----Appdesigner/Datamover/AppEngine Settings------"
       if args.first.nil?
         name = ask("Database name: ").upcase
       else
@@ -17,7 +17,7 @@ module Voodoo
         settings['migration_archive'] = get_path("Archive destination")
       end
 
-      puts "\n### SQR Settings ###"
+      puts "\n----SQR Settings----------------------------------"
       settings['db_username'] = ask("Database username: ") { |q| q.default = "sysadm" }
       settings['ps_home'] = get_path("PS_HOME directory")
 
--- lib/voodoo/commands/config.rb
+++ lib/voodoo/commands/config.rb
@@ -5,7 +5,7 @@ module Voodoo
 
     def self.config
       settings = {}
-      puts "\n### Global Configuration Settings ###"
+      puts "\n----Global Configuration Settings-----------------"
       settings[:ps_home] = get_path("Local tools directory")
       settings[:migration_output_dir] = get_path("Default output directory for migration data")
       Voodoo.write_config_file(settings)
