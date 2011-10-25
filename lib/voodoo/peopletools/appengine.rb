8e67b94d2586edcc6af3e6f3128ae3a548c99b9d
JR Bing <jrbing@gmail.com>
2011-10-25 15:25:26 -0400

    Make running app engines non-blocking

--- lib/voodoo/peopletools/appengine.rb
+++ lib/voodoo/peopletools/appengine.rb
@@ -15,8 +15,24 @@ module Voodoo
       append(:env_password => target.app_password)
       append(:r => '1')
       append(:ae_name => ae_name)
-      puts "Running #{ae_name}..."
+      #puts "Running #{ae_name}..."
+      LOG.info("Running #{ae_name}...")
       call_executable
+    end
+
+    def call_executable
+      LOG.debug("Executable is set to #{@executable}")
+      LOG.debug("Command line options are set to #{@command_line_options.join(" ")}")
+
+      #f = IO.popen(@executable + " " + @command_line_options.join(" "))
+      #f.readlines.each { |line| puts ("#{line.chomp}")}
+      #f.close
+      pid = spawn("start " + @executable + " " + @command_line_options.join(" "))
+      LOG.info("Created background process #{pid} for #{@executable}")
+      Process.detach(pid)
+
+      @command_line_options.clear
+      set_base_parameters
     end
 
   end
