06a2ad8548420114bdb08a28e4815ae02c19b0d6
JR Bing <jr@jrbing.com>
2014-08-19 09:42:50 -0400

    Template fixes

--- lib/voodoo/commands/templates/script.sql
+++ lib/voodoo/commands/templates/script.sql
@@ -1,4 +1,12 @@
 -- Datafix <%= name %>
 -- Created on: <%= Time.now.localtime.strftime("%m/%d/%Y") %>
 
+spool script.out;
+
+-- Environment Information
+select dbname
+     , ownerid
+     , to_char(sysdate,'YYYY/MM/DD HH:MI:SS')
+  from psdbowner;
+
 -- Insert sql inserts/updates/deletes here
